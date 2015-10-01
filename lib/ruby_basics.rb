dddce312392bb8fb133e6be55d1b54696516ac65
Anna Rasshivkina <anna.nigma@yahoo.com>
2015-09-30 23:37:26 -0400

    Done

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(greet,name)
+  puts "#{greet} #{name}!"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  1 + 2 + 3 + 4
+  "what I type here doesn't matter"
+  "because it's not the final value"
+  true
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  puts "You requested a #{topping} pizza"
+  topping
 end
